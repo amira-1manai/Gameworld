2f9a9802e8e725602fd8176e6fdfef1623f5a0c9
Amira Manai <manai.amira@esprit.tn>
2024-11-05 22:57:28 +0100

    sponsoring

--- mainwindow.h
+++ mainwindow.h
@@ -1,16 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-#include "evenement.h"
-#include <QDialog>
-#include <QObject>
-#include <QMainWindow>
 
-#include <QPainter>
-#include <QPdfWriter>
-#include <QDesktopServices>
-#include <QUrl>
-
-#include <QVector>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -25,42 +15,7 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void on_pushButton_ajouter_E_clicked();
-
-    void on_pushButton_supprimer_E_clicked();
-
-    void on_tableView_clicked(const QModelIndex &index);
-
-    void on_pushButton_modifier_E_clicked();
-
-    void on_pushButton_rafraichir_E_clicked();
-
-    void on_pushButton_quitter_clicked();
-
-    void on_pushButton_rafraichir_clicked();
-
-    void on_pushButton_2_quitter_clicked();
-
-    void on_pushButton_tri_codee_clicked();
-
-    void on_pushButton_tri_nom_clicked();
-
-    void on_pushButton_tri_prix_clicked();
-
-    void on_pushButton_chercher_type_clicked();
-
-    void on_pushButton_chercher_ville_clicked();
-
-    void on_pushButton_pdf_clicked();
-
-    void makePlot();
-    QVector<double> Statistique();
-
-    void on_pushButton_statistique_clicked();
-
 private:
     Ui::MainWindow *ui;
-    evenement tmp;
 };
 #endif // MAINWINDOW_H
